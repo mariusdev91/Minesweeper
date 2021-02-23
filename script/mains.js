020a95b6b70d3589581b8a4221f3d2678c357e61
Marius Erdei <67762314+mariusdev91@users.noreply.github.com>
2021-02-23 19:26:13 +0200

    commit

--- script/mains.js
+++ script/mains.js
@@ -8,23 +8,35 @@ function arrayofMines (){
         }
         document.write("<br>");
     }
-}
+}   
 
 function returnMineId() {
     const mine = document.getElementById('board');
-    mine.addEventListener('click', function(e) {
+    mine.addEventListener('mouseup', function(e) {
         let btnId = e.target.getAttribute('id');
-        if (e.target.innerText === '') {
-            e.target.innerText = btnId;
-        } else {
-            e.target.innerText = '';
+        if (typeof e === 'object'){
+            if (e.button === 0) {
+                if (e.target.innerText === '') {
+                    e.target.innerText = btnId;
+                    e.target.style.backgroundColor = "blue";
+                } else {
+                    e.target.innerText = '';
+                    e.target.style.backgroundColor = "white";
+                }
+            } else if (e.button === 2) {
+                e.target.style.backgroundColor = "red";
+            }
         }
+    });
+    mine.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
     });
 }
 
 function startGame() {
     const score = document.getElementById('score');
     const life = document.getElementById('life');
+    const buttons = document.querySelectorAll('button');
     score.innerText += " " + 3;
     life.innerText += " " + 1;
     returnMineId();
